3721dc3a39b5a9a243e5dbe250b8de81edcbbbc5
Alexandre Volts <alexandre.cochet@epitech.eu>
2020-03-21 15:59:36 +0100

    feat(server/auth) Login & Registration system to firebase made.

---
+++ server/API.md
@@ -0,0 +1,12 @@
+# API.md
+
+## Users
+
+### 1) Register a user
+
+
+### 2) Login a user
+
+
+### 3) Logout a user
+
--- server/src/Server.ts
+++ server/src/Server.ts
@@ -1,7 +1,10 @@
-import express = require("express");
 import http = require("http");
 import fs = require("fs");
-import firebase = require("firebase");
+import express = require("express");
+import bodyParser = require("body-parser");
+import firebase from "firebase/app";
+import "firebase/database";
+import "firebase/auth";
 import Database from "./Database";
 import APIController from "./APIController";
 import UsersController from "./UsersController";
@@ -12,12 +15,14 @@ class Server
 	private app = express();
 	private server:http.Server;
 	private config:any = JSON.parse(fs.readFileSync("config.json", "utf8"));
+	private firebaseApp:firebase.app.App;
 
 	constructor(port:number)
 	{
 		this.PORT = port;
+		this.app.use(bodyParser.json());
 		this.server = this.app.listen(this.PORT, this.initialise);
-		firebase.initializeApp(this.config.firebase);
+		this.firebaseApp = firebase.initializeApp(this.config.firebase);
 	}
 
 	private initialise = (err:string):void =>
@@ -30,11 +35,25 @@ class Server
 		}
 		console.log(`Server is running on port ${this.PORT}.`);
 		controllers = [
-			new UsersController(this.app, firebase.database())
+			//new UsersController(this.app, firebase.database())
 		];
-		this.app.get("/", (req:express.Request, res:express.Response) => {
-			req;
-			res;
+		this.app.post("/register", (req:express.Request, res:express.Response) => {
+			if (!req.body.email || !req.body.password)
+				res.status(400).send("Email or password is invalid.");
+			this.firebaseApp.auth()
+			.createUserWithEmailAndPassword(req.body.email, req.body.password)
+			.catch((err:firebase.auth.Error) => {
+				res.status(400).send(err.message);
+			});
+		});
+		this.app.post("/login", (req:express.Request, res:express.Response) => {
+			if (!req.body.email || !req.body.password)
+				res.status(400).send("Email or password is invalid.");
+			this.firebaseApp.auth()
+			.signInWithEmailAndPassword(req.body.email, req.body.password)
+			.catch((err:firebase.auth.Error) => {
+				res.status(400).send(err.message);
+			});
 		});
 	}
 }
